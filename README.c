9a85545191a5c2b9877d15b338c6da4c9c3779af
Sandro687 <34144569+Sandro687@users.noreply.github.com>
2017-11-30 21:27:56 -0200

    Create README.c

---
+++ README.c
@@ -0,0 +1,108 @@
+//# Final-IP
+//Resolução da final da prova de Introdução à Programação, 2015.2.
+//Galera, essa e a minha primeira vez usando github. O usaremos para facilitar a criação do codigo. Todos podem ve-lo e altera-lo, apos a
+//vizualizaçao de erros e relacionados.
+//Irei separar o codigo por passos. Ao final criaremos outro codigo, a uniao de todos os passos enviados pelo git. Tentem o maximo que pu-
+//derem. Farei o mesmo!
+
+//Passo 1 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 2 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 3 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 4 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 5 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 6 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 7 - Codigo fonte:
+
+
+
+
+
+
+
+
+
+
+
+
+//Passo 8 - Codigo fonte:
+
+
+
+
+
+
+
+
+
