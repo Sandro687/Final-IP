17d9b4eafa41753545a003ff9ee3adf22cf513bd
Sandro687 <34144569+Sandro687@users.noreply.github.com>
2017-12-01 00:32:03 -0200

    Create Codigo_Final.c

---
+++ Codigo_Final.c
@@ -0,0 +1,14 @@
+//Aqui sera posto o codigo final, isto e, a juncao de todos os passos. Sintam-se livres para edita-lo como quiserem, apenas lembrem-se
+//de clicar no "Create Pull Request", ou o codigo aparecera editado para você, mas nao para os outros
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main (void){
+
+
+
+  return 0;
+}
