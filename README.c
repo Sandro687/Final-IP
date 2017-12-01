50fb7fdf4e3ed0e08f5eae9e47cb916a39f4fed3
Sandro687 <34144569+Sandro687@users.noreply.github.com>
2017-12-01 09:44:56 -0200

    Update README.c

--- README.c
+++ README.c
@@ -1,9 +1,10 @@
 //# Final-IP
-//Resolução da final da prova de Introdução à Programação, 2015.2.
-//Galera, essa e a minha primeira vez usando github. O usaremos para facilitar a criação do codigo. Todos podem ve-lo e altera-lo, apos a
+//Resolucao da final da prova de Introducao a Programacao, 2015.2.
+//Galera, essa e a minha primeira vez usando github. O usaremos para facilitar a criacao do codigo. Todos podem ve-lo e altera-lo, apos a
 //vizualizaçao de erros e relacionados.
 //Irei separar o codigo por passos. Ao final criaremos outro codigo, a uniao de todos os passos enviados pelo git. Tentem o maximo que pu-
 //derem. Farei o mesmo!
+//Apenas lembrem-se de "Create Pull Request", ou o codigo nao ira mudar.
 
 //Passo 1 - Codigo fonte:
 
