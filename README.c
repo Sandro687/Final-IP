6fb7001b2faa015a0a06920a32b171836498969a
Sandro687 <34144569+Sandro687@users.noreply.github.com>
2017-12-02 14:24:33 -0200

    Update README.c

--- README.c
+++ README.c
@@ -8,30 +8,17 @@
 
 //Passo 1 - Codigo fonte:
 
-typedef struct 
-        { 
-        char descricaoRegistro1 [30];
-        char descricaoRegistro2 [30];
-        char descricaoRegistro3 [30];
-        char descricaoRegistro4 [30];
-        char descricaoRegistro5 [30];
-        char descricaoRegistro6 [30];
-        char descricaoRegistro7 [30];
-        char descricaoRegistro8 [30];
-        char descricaoRegistro9 [30];
-        char descricaoRegistro10 [30];
-        char descricaoRegistro11 [30];
-        char descricaoRegistro12 [30];
-        char descricaoRegistro13 [30];
-        char descricaoRegistro14 [30];
-        char descricaoRegistro15 [30];
-        int RegistrosX [15];
-        int RegistrosY [15];
-        int RegistroCadastrX [15];
-        int RegistroCadastrY[15]
-        int indice [30];
-        } Registro; 
-
+        typedef struct //A struct "Registro" sera global.
+                { 
+                char descricaoRegistro [450];
+                int RegistrosX [15];
+                int RegistrosY [15];
+                int RegistroCadastrX [15];
+                int RegistroCadastrY[15]
+                int indice [30];
+                } Registro; 
+        arr[30]    //O "arr[30]" sera global.
+        
 //Passo 2 - Codigo fonte:
 
         double PontosProximos (double raio){
@@ -116,116 +103,19 @@ void sair()
 
 int CadstrOcorr()
 {
-    int *Registro.RegistrosX [15];               //Faz-se necessario o uso de ponteiros, ou a variavel sera descartada apos o uso da funcao.
-    int *Registro.RegistrosY [15];
-    char *descricaoRegistro1 [30];
-    char *descricaoRegistro2 [30];
-    char *descricaoRegistro3 [30];
-    char *descricaoRegistro4 [30];
-    char *descricaoRegistro5 [30];
-    char *descricaoRegistro6 [30];
-    char *descricaoRegistro7 [30];
-    char *descricaoRegistro8 [30];
-    char *descricaoRegistro9 [30];
-    char *descricaoRegistro10 [30];
-    char *descricaoRegistro11 [30];
-    char *descricaoRegistro12 [30];
-    char *descricaoRegistro13 [30];
-    char *descricaoRegistro14 [30];
-    char *descricaoRegistro15 [30];
-    
+    int i = 0;
+    char descricaoRegistroTemp[30]
     while (1){
             printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((RegistrosX[0] == 0) && (RegistrosY == [0]))
+            scanf("%d%d, &Registro.RegistrosX[i], &Registro.RegistrosY[i]);
+            if ((Registro.RegistrosX[i] == 0) && (Registro.RegistrosY [i] == 0))
                     break;
-            scanf("%d%d, &RegistrosX[0], &RegistrosY[0]);
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[0], RegistrosY[0]);
-            fgets(descricaoRegistro1, sizeof(descricaoRegistro1), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((RegistrosX[1] == 0) && (RegistrosY[1] == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[1], &RegistrosY[1]);
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[1], RegistrosY[1]);
-            fgets(descricaoRegistro2, sizeof(descricaoRegistro2), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[2], &RegistrosY[2]);
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[2], RegistrosY[2]);
-            fgets(descricaoRegistro3, sizeof(descricaoRegistro3), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[3], &RegistrosY[3]);
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[3], RegistrosY[3]);
-            fgets(descricaoRegistro4, sizeof(descricaoRegistro4), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[3], &RegistrosY[3]);
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[3], RegistrosY[3]);
-            fgets(descricaoRegistro4, sizeof(descricaoRegistro4), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            scanf("%d%d, &RegistrosX[4], &RegistrosY[4]);
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[4], RegistrosY[4]);
-            fgets(descricaoRegistro5, sizeof(descricaoRegistro5), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[5], &RegistrosY[5]);
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[5], RegistrosY[5]);
-            fgets(descricaoRegistro6, sizeof(descricaoRegistro6), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[6], &RegistrosY[6])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[6], RegistrosY[6]);
-            fgets(descricaoRegistro7, sizeof(descricaoRegistro7), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[7], &RegistrosY[7])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[7], RegistrosY[7]);
-            fgets(descricaoRegistro8, sizeof(descricaoRegistro8), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            if ((Registrosx == 0) && (Registrosy == 0))
-                    break;
-            scanf("%d%d, &RegistrosX[8], &RegistrosY[8])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[8], RegistrosY[8]);
-            fgets(descricaoRegistro9, sizeof(descricaoRegistro9), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            scanf("%d%d, &RegistrosX[9], &RegistrosY[9])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[9], RegistrosY[9]);
-            fgets(descricaoRegistro10, sizeof(descricaoRegistro10), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            scanf("%d%d, &RegistrosX[0], &RegistrosY[0])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[0], RegistrosY[0]);
-            fgets(descricaoRegistro1, sizeof(descricaoRegistro1), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            scanf("%d%d, &RegistrosX[0], &RegistrosY[0])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[0], RegistrosY[0]);
-            fgets(descricaoRegistro1, sizeof(descricaoRegistro1), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            scanf("%d%d, &RegistrosX[0], &RegistrosY[0])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[0], RegistrosY[0]);
-            fgets(descricaoRegistro1, sizeof(descricaoRegistro1), stdin);
-            printf("Insira as coordenadas (x,y), respectivamente.\n");
-            scanf("%d%d, &RegistrosX[0], &RegistrosY[0])
-            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", RegistrosX[0], RegistrosY[0]);
-            fgets(descricaoRegistro1, sizeof(descricaoRegistro1), stdin);
-            
+            printf("Insira a descricao das coordenadas (%d, %d) inseridas\n.", Registro.RegistrosX[i], Registro.RegistrosY[i]);
+            fgets(descricaoRegistroTemp, sizeof(descricaoRegistroTemp), stdin);
+            arr[i] = strlen(descricaoRegistroTemp);     //"arr[i]" sera usado para delimitar as strings salvas na variavel string global.
+            strcat(descricaoRegistro, descricaoRegistroTemp);
+            i++;
 }
-
-
-
-
-
-
-
-
-
 
 //Passo 5 - Codigo fonte:
 
